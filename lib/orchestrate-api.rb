af92d0ecec3cce81d38a597a242152833992737a
James Carrasquer <jimcar@aracnet.com>
2014-02-23 16:41:40 -0800

    Convert tabs to spaces

--- lib/orchestrate-api.rb
+++ lib/orchestrate-api.rb
@@ -9,14 +9,14 @@ module Orchestrate
   The <b>Wrapper[API/Wrapper.html]</b> class is used to setup the client and make HTTP requests.
 =end
 
-	module API
+  module API
 
-		# require "active_support/core_ext"
+    # require "active_support/core_ext"
 
-		require "orchestrate_api/procedural"
+    require "orchestrate_api/procedural"
     require "orchestrate_api/wrapper"
-		require "orchestrate_api/request"
-		require "orchestrate_api/response"
+    require "orchestrate_api/request"
+    require "orchestrate_api/response"
 
-	end
+  end
 end
