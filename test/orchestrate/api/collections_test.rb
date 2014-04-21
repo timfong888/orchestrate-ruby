f385c4da993b9752fd5c91f0dc13d7e6713b2ddf
Matthew Lyon <matthew@lyonheart.us>
2014-04-21 15:18:06 -0700

    tests for collections listing

--- test/orchestrate/api/collections_test.rb
+++ test/orchestrate/api/collections_test.rb
@@ -18,4 +18,29 @@ class CollectionTest < MiniTest::Unit::TestCase
     assert response.body.empty?
   end
 
+  def test_lists_collection_without_params
+    @stubs.get("/#{@collection}") do |env|
+      assert_authorization @basic_auth, env
+      [200, response_headers, '{"count":3, "next":"blah", "results":[]}']
+    end
+
+    response = @client.list({collection:@collection})
+    assert_equal 200, response.status
+    assert_equal 3, response.body['count']
+    assert response.body['results']
+  end
+
+  def test_lists_collections_with_params_passes_them
+    @stubs.get("/#{@collection}") do |env|
+      assert_authorization @basic_auth, env
+      assert_equal "1", env.params['limit']
+      [ 200, response_headers, '{"count":1, "next":"blah", "results":[]}' ]
+    end
+
+    response = @client.list({collection:@collection, path:"?limit=1"})
+    assert_equal 200, response.status
+    assert_equal 1, response.body['count']
+    assert response.body['results']
+  end
+
 end
