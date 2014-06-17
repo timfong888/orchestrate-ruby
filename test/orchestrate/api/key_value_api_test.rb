e7962217e2e5d63af4525d981731bb5794614440
Matthew Lyon <matthew@lyonheart.us>
2014-06-17 11:40:12 -0700

    resolve test class name conflict

--- test/orchestrate/api/key_value_api_test.rb
+++ test/orchestrate/api/key_value_api_test.rb
@@ -1,6 +1,6 @@
 require_relative "../../test_helper"
 
-class KeyValueTest < MiniTest::Unit::TestCase
+class KeyValueAPITest < MiniTest::Unit::TestCase
   def setup
     @collection = 'test'
     @key = 'keyname'
