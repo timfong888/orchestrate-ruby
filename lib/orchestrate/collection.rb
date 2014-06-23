d30954195d1c43931226dffd2667378414568dc0
Matthew Lyon <matthew@lyonheart.us>
2014-06-23 16:27:32 -0700

    Collection gets yardoc

--- lib/orchestrate/collection.rb
+++ lib/orchestrate/collection.rb
@@ -1,15 +1,16 @@
 module Orchestrate
   class Collection
 
+    # @return [Orchestrate::Application] The application this collection belongs to.
     attr_reader :app
 
+    # @return [String] The name of this collection.
     attr_reader :name
 
-    def to_s
-      "#<Orchestrate::Collection name=#{name} api_key=#{app.api_key[0..7]}...>"
-    end
-    alias :inspect :to_s
-
+    # Instantiate the Collection
+    # @param app [Orchestrate::Client, Orchestrate::Application] The application the Collection belongs to.
+    # @param collection_name [#to_s] The name of the collection.
+    # @return Orchestrate::Collection
     def initialize(app, collection_name)
       if app.kind_of? Orchestrate::Client
         @app = Application.new(app)
@@ -19,10 +20,17 @@ module Orchestrate
       @name = collection_name.to_s
     end
 
+    # Deletes the collection.
+    # @return Orchestrate::API::Response
     def destroy!
       app.client.delete_collection(name)
     end
 
+    # @!group KeyValue getters, setters
+
+    # [Retrieves a KeyValue item by key](http://orchestrate.io/docs/api/#key/value/get).
+    # @param key_name [#to_s] The key of the item
+    # @return [Orchestrate::KeyValue, nil] The KeyValue item if found, nil if not.
     def [](key_name)
       begin
         KeyValue.load(self, key_name)
@@ -31,10 +39,26 @@ module Orchestrate
       end
     end
 
+    # [Sets a KeyValue item by key](http://orchestrate.io/docs/api/#key/value/put-\(create/update\)).
+    # @param key_name [#to_s] The key of the item.
+    # @param value [#to_json] The value to store at the key.
+    # @return [value] The item provided for 'value'.
+    # @note Ruby will return the value provided to `something=` methods.  If you want the KeyValue returned, use #set.
+    # @raise Orchestrate::API::BadRequest the body is not valid JSON.
+    # @see #set
     def []=(key_name, value)
       set(key_name, value)
     end
 
+    # [Sets a KeyValue item by key](http://orchestrate.io/docs/api/#key/value/put-\(create/update\)).
+    # @param key_name [#to_s] The key of the item.
+    # @param value [#to_json] The value to store at the key.
+    # @param condition [nil, false, #to_s] (see Orchestraate::Client#put)
+    # @return [Orchestrate::KeyValue] The new KeyValue item.
+    # @raise Orchestrate::API::BadRequest the body is not valid JSON.
+    # @raise Orchestrate::API::VersionMismatch a String condition was provided, but does not match the ref for the current value.
+    # @raise Orchestrate::API::AlreadyPresent a false condition was provided, but a value already exists for this key
+    # @see #[]=
     def set(key_name, value, condition=nil)
       resp = app.client.put(name, key_name, value, condition)
       kv = KeyValue.new(self, key_name, resp)
@@ -42,13 +66,29 @@ module Orchestrate
       kv
     end
 
+    # [Creates a KeyValue item by key, if the key doesn't have
+    # a value](http://orchestrate.io/docs/api/#key/value/put-\(create/update\)).
+    # @param key_name [#to_s] The name of the key
+    # @param value [#to_json] The value to store at the key
+    # @return [Orchestrate::KeyValue, nil] The KeyValue if created, false if not
     def create(key_name, value)
       set(key_name, value, false)
     rescue Orchestrate::API::AlreadyPresent
       false
     end
 
+    # @!endgroup
+
+
     include Enumerable
+    # @!group KeyValue enumerators
+
+    # Lazily iterates over each KeyValue item in the collection.  Used as the basis for Enumerable methods.
+    # Items are provided in lexicographically sorted order by key name.
+    # @yieldparam [Orchestrate::KeyValue] key_value The KeyValue item
+    # @example
+    #   keys = collection.take(20).map(&:key)
+    #   # returns the first 20 keys in the collection.
     def each
       return enum_for(:each) unless block_given?
       response = app.client.list(name)
@@ -60,6 +100,14 @@ module Orchestrate
         response = response.next_results
       end
     end
+
+    # @!endgroup
+
+    # @!visibility private
+    def to_s
+      "#<Orchestrate::Collection name=#{name} api_key=#{app.api_key[0..7]}...>"
+    end
+    alias :inspect :to_s
 
   end
 end
