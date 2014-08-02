8e983cb7964a20c4bd150f7d18b8719cddc471e5
Matthew Lyon <matthew@lyonheart.us>
2014-08-02 14:48:26 -0700

    Events, create w/ timestamp

--- lib/orchestrate/events.rb
+++ lib/orchestrate/events.rb
@@ -7,23 +7,53 @@ module Orchestrate
     end
 
     def [](event_type)
-      @types[event_type.to_s] ||= EventsStem.new(@kv_item, event_type)
+      @types[event_type.to_s] ||= EventsType.new(@kv_item, event_type)
     end
   end
 
-  class EventsStem
-    attr_accessor :kv_item
-    attr_accessor :type
+  class EventsType
+    attr_reader :kv_item
+    attr_reader :type
     def initialize(kv_item, event_type)
       @kv_item = kv_item
       @type = event_type.to_s
     end
 
     def <<(body)
-      response = kv_item.perform(:post_event, type, body)
-      Event.from_bodyless_response(self, body, response)
+      Range.new(self).push(body)
     end
     alias :push :<<
+
+    def [](bounds)
+      Range.new(self, bounds)
+    end
+
+    class Range
+      attr_reader :type
+      attr_reader :type_name
+      attr_reader :kv_item
+      def initialize(type, bounds=nil)
+        @type = type
+        @type_name = type.type
+        @kv_item = type.kv_item
+        if bounds.kind_of?(Hash)
+          @bounds = bounds
+        else
+          @bounds = {limit: 100}
+          @bounds[:start] = bounds
+        end
+      end
+
+      def perform(api_method, *args)
+        kv_item.perform(api_method, type_name, *args)
+      end
+
+      def <<(body)
+        response = perform(:post_event, body, @bounds[:start])
+        Event.from_bodyless_response(type, body, response)
+      end
+      alias :push :<<
+    end
   end
 
   class Event
@@ -61,9 +91,9 @@ module Orchestrate
     def load_from_response(response)
       response.on_complete do
         @ref = response.ref if response.respond_to?(:ref)
-        vers, coll, k, e, t, ts, ord = response.headers['Location'].split('/')
-        @timestamp = ts.to_i
-        @ordinal = ord.to_i
+        loc = response.headers['Location'].split('/')
+        @timestamp = loc[6].to_i
+        @ordinal = loc[7].to_i
         @time = Time.at(@timestamp / 1000.0)
         @last_request_time = response.request_time
       end
--- test/orchestrate/event_test.rb
+++ test/orchestrate/event_test.rb
@@ -8,14 +8,15 @@ class EventTest < MiniTest::Unit::TestCase
   end
 
   def test_creates_event_without_timestamp
-    body = {place: 'Home'}
+    body = {"place" => 'Home'}
     time = Time.now
     timestamp = (time.to_f * 1000).to_i
     ord = 3
     type = 'checkins'
     ref = nil
     @stubs.post("/v0/items/#{@kv.key}/events/#{type}") do |env|
-      loc = "/v0/items/#{@kv.key}/events#{type}/#{timestamp}/#{ord}"
+      assert_equal body, JSON.parse(env.body)
+      loc = "/v0/items/#{@kv.key}/events/#{type}/#{timestamp}/#{ord}"
       ref = make_ref
       [201, response_headers({"Location" => loc, "Etag" => ref}), '']
     end
@@ -24,7 +25,37 @@ class EventTest < MiniTest::Unit::TestCase
     assert_kind_of Orchestrate::Event, event
     assert_equal @kv.key, event.key_name
     assert_equal @kv, event.key
-    assert_equal 'checkins', event.type_name
+    assert_equal type, event.type_name
+    assert_equal @kv.events[:checkins], event.type
+    assert_equal ref, event.ref
+    assert_kind_of Time, event.time
+    assert_in_delta time.to_f, event.time.to_f, 1.1
+    assert_equal timestamp, event.timestamp
+    assert_equal ord, event.ordinal
+    assert_equal "/items/#{@kv.key}/events/#{type}/#{timestamp}/#{ord}", event.path
+    assert_equal body, event.value
+    assert_in_delta Time.now.to_f, event.last_request_time.to_f, 1.1
+  end
+
+  def test_creates_event_with_time
+    body = {"place" => 'Home'}
+    time = Time.now
+    timestamp = (time.to_f * 1000).to_i
+    ord = 6
+    type = 'checkins'
+    ref = nil
+    @stubs.post("/v0/items/#{@kv.key}/events/#{type}/#{timestamp}") do |env|
+      assert_equal body, JSON.parse(env.body)
+      loc = "/v0/items/#{@kv.key}/events/#{type}/#{timestamp}/#{ord}"
+      ref = make_ref
+      [201, response_headers({"Location" => loc, "Etag" => ref}), '']
+    end
+    event = @kv.events[:checkins][time].push(body)
+    @stubs.verify_stubbed_calls
+    assert_kind_of Orchestrate::Event, event
+    assert_equal @kv.key, event.key_name
+    assert_equal @kv, event.key
+    assert_equal type, event.type_name
     assert_equal @kv.events[:checkins], event.type
     assert_equal ref, event.ref
     assert_kind_of Time, event.time
